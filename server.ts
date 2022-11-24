fa4cc36d80f76a4d2eca87665e0a09849cac3f01
roanlinde <65543219+roanlinde@users.noreply.github.com>
2022-11-24 12:25:38 +0200

    Update server.ts
    Added comment

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2014-2022 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
+ * Roan Testing
  */
 import dataErasure from './routes/dataErasure'
 import fs = require('fs')
